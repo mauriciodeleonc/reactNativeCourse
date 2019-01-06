06a42ebdc0fa7409bf9b4f3d2c38b9f7f66bc9d4
Mauricio De León <mauricio.deleon.c13@gmail.com>
2019-01-05 18:21:14 -0600

    First commit

---
+++ albums/index.js
@@ -0,0 +1,13 @@
+//Import library to help create a component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+//Create a component
+const App = () => {
+  return (
+    <Text>Some text</Text>
+  );
+};
+
+//Render it to the device
+AppRegistry.registerComponent('albums', () => App);
